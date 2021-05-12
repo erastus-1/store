23cdfb683ba1004ba377186a95abef6293374835
erastus-1 <erastuskariuki15@gmail.com>
2021-05-12 11:54:01 +0300

    added

--- store/src/actions/PostAction.jsx
+++ store/src/actions/PostAction.jsx
@@ -1,10 +1,11 @@
 import { FETCH_POST, NEW_POST} from './Types';
 
 
-export function fetchPost(){
+export const fetchPost =()=> dispatch => {
         fetch('https://storemanagerapi2.herokuapp.com/api/v2/products')
     .then(res => res.json())
-    .then(data => dispatchEvent({
+    .then(data => 
+        dispatch({
         type: FETCH_POST,
         payload : post
     }));
